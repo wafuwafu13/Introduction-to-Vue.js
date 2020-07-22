f57beaa68932ebb8fc02e643c501f4c125b1df90
wafuwafu13 <mariobaske@i.softbank.jp>
2020-07-22 17:05:24 +0900

    データフローの設計

--- kanban-app/src/store/index.js
+++ kanban-app/src/store/index.js
@@ -6,9 +6,20 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+const state = {
+  auth: {
+    token: null,
+    userId: null
+  },
+  board: {
+    lists: []
+  }
+}
+
 export default new Vuex.Store({
-    getters,
-    actions,
-    mutations,
-    strict: process.env.NODE_ENV !== 'production'
+  state,
+  getters,
+  actions,
+  mutations,
+  strict: process.env.NODE_ENV !== 'production'
 })
